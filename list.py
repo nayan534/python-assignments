3e993c6ddfa9b2e678cf77ede60aef1ce86bc72d
nayan534 <nayanrathod410@gmail.com>
2026-02-08 22:12:52 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,22 @@
+# Create list
+list = [100,200,300,400,500]
+print("Original List:", list)
+
+# Access elements
+print("First element:", list[2])
+
+# Add elements
+list.append(1000)
+print("After Add:", list)
+
+# Remove elements
+list.remove(400)
+print("After Remove:", list)
+
+# Sort list
+list.sort()
+print("Sorted List:", list)
+
+# Reverse list
+list.reverse()
+print("Reversed List:", list)
