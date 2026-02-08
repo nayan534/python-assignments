4672383ee666e1ca29cefafe92bf18fcfb79fe41
nayan534 <nayanrathod410@gmail.com>
2026-02-08 22:15:30 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,15 @@
+t1 = (100,400,700)
+t2 = (36,477,67)
+
+# Access
+print("First element:", t1[0])
+
+# Nested Tuple
+nested = (t1, t2)
+print("Nested Tuple:", nested)
+
+# Repetition
+print("Repeated:", t1 * 2)
+
+# Concatenation
+print("Concatenated:", t1 + t2)
