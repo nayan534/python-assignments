c218cd6fb3470d6d2a06b16842e9a62b32543238
nayan534 <nayanrathod410@gmail.com>
2026-02-08 22:14:56 +0530

    Add files via upload

---
+++ set operation.py
@@ -0,0 +1,9 @@
+A = {56,67,68,60}
+B = {34,45,67,23}
+
+print("Set A:", A)
+print("Set B:", B)
+
+print("Union:", A.union(B))
+print("Intersection:", A.intersection(B))
+print("Difference (A-B):", A.difference(B))
